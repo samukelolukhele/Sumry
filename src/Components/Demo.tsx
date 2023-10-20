4def3b1c52ee0c07fbb3d10fdde8d25747fe85ac
samukelolukhele <samukelo.business@gmail.com>
2023-10-20 16:32:21 +0200

    SETUP: Hero setup.

--- src/Components/Demo.tsx
+++ src/Components/Demo.tsx
@@ -1,5 +1,38 @@
+import { useState, useEffect } from "react";
+
 const Demo = () => {
-  return <div>Demo</div>;
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/* Search */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          action=""
+          className="relative flex justify-center items-center"
+          onSuspend={() => {}}
+        >
+          <img
+            src="/assets/link.svg"
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value=""
+            onChange={() => {}}
+            required
+            className="url_input peer"
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Demo;
--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,5 +1,31 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img
+          src="/assets/logo.svg"
+          alt="sumry_logo"
+          className="w-28 object-contain"
+        />
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/samukelolukhele")}
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Sumry, an open-sorce article summarizer that
+        transforms lengthy articles into clear and concise summaries.
+      </h2>
+    </header>
+  );
 };
 
 export default Hero;
